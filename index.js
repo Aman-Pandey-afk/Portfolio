d033c74d7b50bde249dde767d11d64dc326b7ba1
Aman-Pandey-afk <77021852+Aman-Pandey-afk@users.noreply.github.com>
2022-02-05 17:52:58 +0530

    Updates

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-import './style.css'
+//import './style.css'
 
-import * as THREE from 'three';
+import * as THREE from "https://unpkg.com/three@0.127.0/build/three.module.js";;
 
-import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
-import { Mesh } from 'three';
+import {OrbitControls} from "https://unpkg.com/three@0.127.0/examples/jsm/controls/OrbitControls.js";
 ;
 
 
